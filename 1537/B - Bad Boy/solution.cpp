70c0c5a3c9fa88245feb8349f47a4d24d98a07b5
Samarthkh_17 <samarthkhandelwal1281@gmail.com>
2026-07-27 17:31:23 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,m,i,j;
+        cin>>n>>m>>i>>j;
+ 
+        long long x1,y1,x2,y2;
+        x1 = y1 = 1;
+        x2 = n;
+        y2 = m;
+        cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
+    }
+ 
+    return 0;
+}
