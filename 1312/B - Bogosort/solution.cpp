f37895cdeeb4a4b37568fbac73ddcded61a6c638
Samarthkh_17 <samarthkhandelwal1281@gmail.com>
2026-08-13 23:09:53 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+ 
+        sort(a.rbegin(), a.rend());
+        for (auto i : a)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
