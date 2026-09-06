38f6d20a5931ad35b637a6f0e2ff8e263a1818a6
Samarthkh_17 <samarthkhandelwal1281@gmail.com>
2026-09-06 16:09:47 +0530

    Add AND Sorting [B] from Codeforces

---
+++ 1682/B - AND Sorting/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+ 
+        int ans = (1 << 30) - 1;
+        for (int i = 0; i < n; ++i)
+        {
+            int x;
+            cin >> x;
+            if (x != i)
+                ans &= x;
+        }
+ 
+        cout << ans << endl;
+    }
+ 
+    return 0;
+}
